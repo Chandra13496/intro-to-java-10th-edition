b9e951216129b7b22cef648615f71d8b08c3e34e
Chandra13496 <57440351+Chandra13496@users.noreply.github.com>
2020-03-30 10:20:51 +0530

    Update Exercise_07.java

--- src/Chapter_17/Exercise_07.java
+++ src/Chapter_17/Exercise_07.java
@@ -9,12 +9,7 @@ import java.io.*;
  *      Suppose a file named Exercise17_07.dat has been created using
  *      the ObjectOutputStream. The file contains Loan objects. The Loan
  *      class in Listing 10.2 does not implement Serializable. Rewrite the
- *      Loan class to implement Serializable. Write a program that reads the
- *      Loan objects from the file and displays the total loan amount. Suppose
- *      you don’t know how many Loan objects are there in the file,
- *      use EOFException to end the loop.
- * Created by luizsa on 10/5/14.
- */
+
 public class Exercise_07 {
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
